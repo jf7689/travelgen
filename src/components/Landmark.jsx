9314146aecc300279489e66f3bf39ae3d5518d74
jf7689 <jf7689@rit.edu>
2023-11-14 16:51:52 -0500

    Generating random landmark from json data working

--- src/components/Landmark.jsx
+++ src/components/Landmark.jsx
@@ -1,12 +1,41 @@
+import { useEffect, useState } from "react";
+import data from "../data/data.json";
+
 export function Landmark() {
+    const [landmark, setLandmark] = useState({
+        name: "",
+        photo: "",
+        maps_url: "",
+    });
+    // Get a random landmark from local json
+    function randomLandmark() {
+        const temp = data.landmarks[Math.floor(Math.random() * data.landmarks.length)];
+        setLandmark({
+            name: temp.name,
+            photo: temp.photo,
+            maps_url: temp.maps_url
+        })
+    }
+
+    // Checking to see if varibles are as expected
+    function check() {
+        console.log(landmark);
+    }
+
+    // Generate initial landmark when arriving on site
+    useEffect(() => {
+        randomLandmark();
+    }, [])
+
     return (
         <>
-            <h1>Landmark Name</h1>
-            <a target="_blank" href="https://www.google.com/maps/place/Gardens+by+the+Bay/@1.2848645,103.8615857,16z/data=!4m10!1m2!2m1!1sGardens+by+the+Bay!3m6!1s0x31da1904937e1633:0x62099677b59fca76!8m2!3d1.2815683!4d103.8636132!15sChJHYXJkZW5zIGJ5IHRoZSBCYXlaFCISZ2FyZGVucyBieSB0aGUgYmF5kgEGZ2FyZGVu4AEA!16zL20vMGIwaGI1?hl=en&entry=ttu">18 Marina Gardens Dr, Singapore 018953</a>
+            <h1>{landmark.name}</h1>
+            <a target="_blank" href={landmark.maps_url}>Google Maps</a>
             <div>
-                <img src="https://images.pexels.com/photos/262577/pexels-photo-262577.jpeg" alt="Landmark" width="1280px" height="720px"/>
+                <img src={landmark.photo} alt={`Photo of ${landmark.name}`} width="1280px" height="720px"/>
             </div>
-            <button>New Landmark</button>
+            <button onClick={randomLandmark}>Generate Landmark</button>
+            <button onClick={check}>Check Landmark</button>
         </>
     );
 }
