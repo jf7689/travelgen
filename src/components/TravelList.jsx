b37b9a9646f1b980199967d77360fce064ac809b
jf7689 <jf7689@rit.edu>
2023-11-17 16:45:51 -0500

    Expandable travel list section

--- src/components/TravelList.jsx
+++ src/components/TravelList.jsx
@@ -1,14 +1,25 @@
+import { useState } from "react";
+
 export function TravelList({ landmarkList }) {
+    const [expanded, setExpanded] = useState(false);
+
+    function expand() {
+        setExpanded(!expanded);
+    }
+
     return (
-        <div>
-            {landmarkList.map((location, i) => {
-                return (
-                    <div key={i}>
-                        <h3>{location.name}</h3>
-                        <img src={location.photo} alt={`Photo of ${location.name}`} width="480px" height="270px"/>
-                    </div>
-                );
-            })}
+        <div onClick={expand}>
+            <h2>Travel List</h2>
+            {expanded ? (
+                landmarkList.map((location, i) => {
+                    return (
+                        <div key={i}>
+                            <h3>{location.name}</h3>
+                            <img src={location.photo} alt={`Photo of ${location.name}`} width="480px" height="270px"/>
+                        </div>
+                    );
+                })
+            ) : null}
         </div>
     );
 }
